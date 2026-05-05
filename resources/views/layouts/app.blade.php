e198fdcb143b27079a9c94d0c2482d48fcc0e41f
Denial <kingtylerdenial2005@gmail.com>
2026-05-05 19:55:30 +0530

    Updated logo design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0">
                     <a href="/" class="flex items-center">
-                        <img src="{{ asset('images/image_1777380181903757.png') }}"
+                        <img src="{{ asset('images\logo_3-removebg-preview.png') }}"
                             class="h-16 w-auto bg-white rounded-lg p-1" style="height: 64px; width: auto;"
                             alt="AVI Tailoring">
                     </a>
